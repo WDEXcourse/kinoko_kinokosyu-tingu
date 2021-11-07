f652b13304e4394a9ab9bafec8c4d7cd55e90577
uuzkqexe <mushwithroom@gmail.com>
2021-11-07 18:03:38 +0900

    強敵作りはじめ！！

--- Assets/kinositakariudo/PlayerScript.cs
+++ Assets/kinositakariudo/PlayerScript.cs
@@ -59,8 +59,28 @@ public class PlayerScript : MonoBehaviour
         {
             Walk++;
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            characterController.Move(this.gameObject.transform.forward * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            characterController.Move(this.gameObject.transform.forward * -1 * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            characterController.Move(this.gameObject.transform.right * -1 * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            characterController.Move(this.gameObject.transform.right * MoveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            Walk++;
+        }
         //if (Input.GetKeyDown(KeyCode.UpArrow))
-            characterController.Move(Velocity * Time.deltaTime);
+        characterController.Move(Velocity * Time.deltaTime);
         Velocity.y += Physics.gravity.y * Time.deltaTime;
         if (characterController.isGrounded)
         {
