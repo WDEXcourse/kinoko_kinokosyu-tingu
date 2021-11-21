3df939a5733fa7842253ddbbe1ec4b44f323b1d4
uuzkqexe <mushwithroom@gmail.com>
2021-11-21 18:02:00 +0900

    猫ちゃん♡　強敵製作中
    かぁわぁいぃいぃ♡

--- Assets/StrongEnemyScript.cs
+++ Assets/StrongEnemyScript.cs
@@ -7,7 +7,9 @@ public class StrongEnemyScript : MonoBehaviour
 {
     public GameObject target;
     NavMeshAgent agent;
-    public GameObject EnemyArrow;
+    public GameObject EnemyArrow1;
+    public GameObject EnemyArrow2;
+    public GameObject EnemyArrow3;
     //public GameObject EnemyArrow2;
     //public GameObject EnemyArrow3;
     [SerializeField]
@@ -43,12 +45,22 @@ public class StrongEnemyScript : MonoBehaviour
         //    angle += 20;
         //    transform.rotation = Quaternion.Euler(0, angle, 0);
         //}
-        GameObject EnemyArrows1 = Instantiate(EnemyArrow, transform.position, transform.rotation);
-        GameObject EnemyArrows2 = Instantiate(EnemyArrow, transform.position, transform.rotation);
+        GameObject EnemyArrows1 = Instantiate(EnemyArrow1, transform.position, transform.rotation);
+        GameObject EnemyArrows2 = Instantiate(EnemyArrow2, transform.position, transform.rotation);
+        GameObject EnemyArrows3 = Instantiate(EnemyArrow3, transform.position, transform.rotation);
         EnemyArrows1.transform.position = this.transform.position + new Vector3(0, 3, 0);
         force = this.gameObject.transform.forward * ArrowSpeed;
         EnemyArrows1.GetComponent<Rigidbody>().AddForce(force);
         Destroy(EnemyArrows1.gameObject, 4);
-
+        EnemyArrows2.transform.rotation = Quaternion.Euler(0, 20, 0);
+        EnemyArrows2.transform.position = this.transform.position + new Vector3(5, 3, 0);
+        force = this.gameObject.transform.forward * ArrowSpeed;
+        EnemyArrows2.GetComponent<Rigidbody>().AddForce(force);
+        Destroy(EnemyArrows2.gameObject, 4);
+        EnemyArrows3.transform.rotation = Quaternion.Euler(0, -20, 0);
+        EnemyArrows3.transform.position = this.transform.position + new Vector3(-5, 3, 0);
+        force = this.gameObject.transform.forward * ArrowSpeed;
+        EnemyArrows3.GetComponent<Rigidbody>().AddForce(force);
+        Destroy(EnemyArrows3.gameObject, 4);
     }
 }
