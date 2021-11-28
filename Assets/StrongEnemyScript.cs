2163862897c710c16d6cc455b942f5fb06178d65
uuzkqexe <mushwithroom@gmail.com>
2021-11-28 18:02:08 +0900

    強敵の矢製作中

--- Assets/StrongEnemyScript.cs
+++ Assets/StrongEnemyScript.cs
@@ -27,6 +27,7 @@ public class StrongEnemyScript : MonoBehaviour
     void Update()
     {
         agent.destination = target.transform.position;
+        Arrow(EnemyArrow1, ArrowSpeed,new Vector3(0,3,0),0);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -39,28 +40,33 @@ public class StrongEnemyScript : MonoBehaviour
     }
     void ArrowAttack()
     {
-        //for (int i = 0; i <= 2; i++) 
-        //{
-        //    float angle = transform.rotation.y;
-        //    angle += 20;
-        //    transform.rotation = Quaternion.Euler(0, angle, 0);
-        //}
         GameObject EnemyArrows1 = Instantiate(EnemyArrow1, transform.position, transform.rotation);
         GameObject EnemyArrows2 = Instantiate(EnemyArrow2, transform.position, transform.rotation);
         GameObject EnemyArrows3 = Instantiate(EnemyArrow3, transform.position, transform.rotation);
+
         EnemyArrows1.transform.position = this.transform.position + new Vector3(0, 3, 0);
         force = this.gameObject.transform.forward * ArrowSpeed;
         EnemyArrows1.GetComponent<Rigidbody>().AddForce(force);
         Destroy(EnemyArrows1.gameObject, 4);
+
         EnemyArrows2.transform.rotation = Quaternion.Euler(0, 20, 0);
         EnemyArrows2.transform.position = this.transform.position + new Vector3(5, 3, 0);
         force = this.gameObject.transform.forward * ArrowSpeed;
         EnemyArrows2.GetComponent<Rigidbody>().AddForce(force);
         Destroy(EnemyArrows2.gameObject, 4);
+
         EnemyArrows3.transform.rotation = Quaternion.Euler(0, -20, 0);
         EnemyArrows3.transform.position = this.transform.position + new Vector3(-5, 3, 0);
         force = this.gameObject.transform.forward * ArrowSpeed;
         EnemyArrows3.GetComponent<Rigidbody>().AddForce(force);
         Destroy(EnemyArrows3.gameObject, 4);
     }
+    void Arrow(GameObject DIY, float Speed, Vector3 Position, float Rotate)
+    {
+        DIY.transform.position = this.transform.position + Position;
+        DIY.transform.rotation = Quaternion.Euler(0, Rotate, 0);
+        DIY.GetComponent<Rigidbody>().AddForce(force);
+        force = this.gameObject.transform.forward * Speed;
+        Destroy(DIY.gameObject, 4);
+    }   
 }
