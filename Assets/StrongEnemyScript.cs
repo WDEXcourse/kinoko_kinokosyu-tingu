9b990e480a98a02acc27ade11a6b128f7d37e698
uuzkqexe <mushwithroom@gmail.com>
2021-12-05 18:03:14 +0900

    強敵製作苦戦中............😿😿😿😿😿😿😿😿😿😿😿😿😿

--- Assets/StrongEnemyScript.cs
+++ Assets/StrongEnemyScript.cs
@@ -27,7 +27,8 @@ public class StrongEnemyScript : MonoBehaviour
     void Update()
     {
         agent.destination = target.transform.position;
-        Arrow(EnemyArrow1, ArrowSpeed,new Vector3(0,3,0),0);
+        /////// Arrow(EnemyArrow1, ArrowSpeed,new Vector3(0,3,0),0);
+        ArrowAttack();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -40,33 +41,25 @@ public class StrongEnemyScript : MonoBehaviour
     }
     void ArrowAttack()
     {
-        GameObject EnemyArrows1 = Instantiate(EnemyArrow1, transform.position, transform.rotation);
-        GameObject EnemyArrows2 = Instantiate(EnemyArrow2, transform.position, transform.rotation);
-        GameObject EnemyArrows3 = Instantiate(EnemyArrow3, transform.position, transform.rotation);
+        GameObject EnemyArrows1 = Instantiate(EnemyArrow1);
+        GameObject EnemyArrows2 = Instantiate(EnemyArrow2);
+        GameObject EnemyArrows3 = Instantiate(EnemyArrow3);
 
-        EnemyArrows1.transform.position = this.transform.position + new Vector3(0, 3, 0);
-        force = this.gameObject.transform.forward * ArrowSpeed;
-        EnemyArrows1.GetComponent<Rigidbody>().AddForce(force);
-        Destroy(EnemyArrows1.gameObject, 4);
-
-        EnemyArrows2.transform.rotation = Quaternion.Euler(0, 20, 0);
-        EnemyArrows2.transform.position = this.transform.position + new Vector3(5, 3, 0);
-        force = this.gameObject.transform.forward * ArrowSpeed;
-        EnemyArrows2.GetComponent<Rigidbody>().AddForce(force);
-        Destroy(EnemyArrows2.gameObject, 4);
-
-        EnemyArrows3.transform.rotation = Quaternion.Euler(0, -20, 0);
-        EnemyArrows3.transform.position = this.transform.position + new Vector3(-5, 3, 0);
-        force = this.gameObject.transform.forward * ArrowSpeed;
-        EnemyArrows3.GetComponent<Rigidbody>().AddForce(force);
-        Destroy(EnemyArrows3.gameObject, 4);
+        //EnemyArrows2.transform.rotation = Quaternion.Euler(0, 20, 0);
+        //EnemyArrows2.transform.position = this.transform.position + new Vector3(5, 3, 0);
+        //force = this.gameObject.transform.forward * ArrowSpeed;
+        //EnemyArrows2.GetComponent<Rigidbody>().AddForce(force);
+        //Destroy(EnemyArrows2.gameObject, 4);
+        Arrow(EnemyArrow1, ArrowSpeed, new Vector3(0, 3, 0), 0);
+        Arrow(EnemyArrow2, ArrowSpeed, new Vector3(5, 3, 0), 20);
+        Arrow(EnemyArrow3, ArrowSpeed, new Vector3(-5, 3, 0), -20);
     }
     void Arrow(GameObject DIY, float Speed, Vector3 Position, float Rotate)
     {
         DIY.transform.position = this.transform.position + Position;
         DIY.transform.rotation = Quaternion.Euler(0, Rotate, 0);
-        DIY.GetComponent<Rigidbody>().AddForce(force);
         force = this.gameObject.transform.forward * Speed;
+        DIY.GetComponent<Rigidbody>().AddForce(force);
         Destroy(DIY.gameObject, 4);
     }   
 }
