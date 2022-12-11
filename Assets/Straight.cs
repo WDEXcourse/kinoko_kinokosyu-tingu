a8391089a15fb84a62e3b883d19218c4904266b0
uuzkqexe <mushwithroom@gmail.com>
2022-12-11 18:09:55 +0900

    しりとりしようぜ！！　お前ボールな！
    ルービックキューブ！！

--- Assets/Straight.cs
+++ Assets/Straight.cs
@@ -11,11 +11,14 @@ public class Straight : MonoBehaviour
     public GameObject target;
     public GameObject MyRotato;
     public float rot;
+    public float i;
     Vector3 forse;
     // Start is called before the first frame update
     void Start()
     {
+        i = 1;
         MyRotato = transform.root.gameObject;
+        //StartCoroutine("timer");
         //target = GameObject.FindGameObjectWithTag("ya2");
     }
 
@@ -26,13 +29,29 @@ public class Straight : MonoBehaviour
         m_shotTimer += Time.deltaTime;
         if (m_shotTimer < m_shotInterval) return;
         m_shotTimer = 0;
+        StartCoroutine("timer");
         //Instantiate(target, transform.position, Quaternion.identity);
+        //GameObject targets = Instantiate(target) as GameObject;
+        //targets.transform.position = this.transform.position + new Vector3(0, -3, 0);
+        //target.transform.localPosition = transform.position;
+        //target.transform.localRotation = MyRotato.transform.rotation;
+        //forse = target.transform.forward * m_shotSpeed;
+        //Debug.Log(forse);
+        //Debug.Log(i);
+        //i++;
+        //targets.GetComponent<Rigidbody>().AddForce(forse);
+    }
+    IEnumerator timer()
+    {
         GameObject targets = Instantiate(target) as GameObject;
         //targets.transform.position = this.transform.position + new Vector3(0, -3, 0);
         target.transform.localPosition = transform.position;
         target.transform.localRotation = MyRotato.transform.rotation;
         forse = target.transform.forward * m_shotSpeed;
-        Debug.Log(forse);
+        //Debug.Log(forse);
+        //Debug.Log(i);
+        //i++;
         targets.GetComponent<Rigidbody>().AddForce(forse);
+        yield return new WaitForSeconds(1);
     }
 }
